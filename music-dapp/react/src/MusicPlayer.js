2c5416b3cd80eb4b5199c6c28339ae24803ea79d
henry190927 <56784043+henry190927@users.noreply.github.com>
2021-06-26 23:41:40 +0800

    remove test button

--- music-dapp/react/src/MusicPlayer.js
+++ music-dapp/react/src/MusicPlayer.js
@@ -32,10 +32,10 @@ import QueueMusicIcon from '@material-ui/icons/QueueMusic';
 import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const test = [
-  {id: 0, name: "Silence", artist: "Khalid", onChoose: true, onPlay: false, src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Marshmello%20-%20Silence%20ft.%20Khalid.mp3'},
-  {id: 1, name: "Fireproof", artist: "VAX, Teddy Sky", onChoose: false, onPlay: false, src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/VAX%20-%20Fireproof%20Feat%20Teddy%20Sky.mp3'},
-]
+// const test = [
+//   {id: 0, name: "Silence", artist: "Khalid", onChoose: true, onPlay: false, src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Marshmello%20-%20Silence%20ft.%20Khalid.mp3'},
+//   {id: 1, name: "Fireproof", artist: "VAX, Teddy Sky", onChoose: false, onPlay: false, src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/VAX%20-%20Fireproof%20Feat%20Teddy%20Sky.mp3'},
+// ]
 
 const useStyles = makeStyles((theme) => ({
   iconPaper: {
@@ -405,7 +405,7 @@ export default function MusicPlayer(props) {
 
   return (
     <Toolbar>
-      <Button
+      {/* <Button
         onClick={() => {
           let music = {
             id: test[0].id,
@@ -444,7 +444,7 @@ export default function MusicPlayer(props) {
         }} 
       >
         click 2
-      </Button>
+      </Button> */}
       <Paper className={clsx(classes.iconPaper, onplayState && spinIconClasses.iconPaper_rotate)} 
         style={{
           transform: `rotate(-${audio.currentTime * 45 % 360}deg)`,
