9f5473e32f484b595ca419ed061fb7625207c883
woodcutter-eric <yingchiao.chen@gmail.com>
2021-06-28 19:44:57 +0800

    document ipfs functions

--- music-dapp/react/src/ipfs/download.js
+++ music-dapp/react/src/ipfs/download.js
@@ -1,23 +1,21 @@
-const { create, CID } = require("ipfs-http-client");
+const { create } = require("ipfs-http-client");
 const all = require("it-all");
 const uint8Array = require("uint8arrays");
 
+/*
+ * Download Audio from IPFS.
+ *
+ * @param[in] cid: The IPFS hash
+ * @param[out] audio: The downloaded audio data.
+ */
 export const downloadAudioIPFS = async (cid) => {
   // connect to the default API address http://localhost:5001
+  // const node = create('http://host.docker.internal:5001');
   const node = create("http://127.0.0.1:5001");
-
-  // const cid = 'QmTs711cqrjHLfRifJ3sVq9uCfYcmBTXQYgQtQGETLqbFy';
-  cid = cid || "QmTs711cqrjHLfRifJ3sVq9uCfYcmBTXQYgQtQGETLqbFy";
-
-  console.log(cid);
 
   const dataArr = uint8Array.concat(await all(node.cat(cid)));
 
   return URL.createObjectURL(
     new Blob([dataArr], { type: "audio/mpeg" } /* (1) */)
   );
-
-  // document.getElementById('music-play').src = URL.createObjectURL(
-  //   new Blob([dataArr], { type: 'audio/mpeg' } /* (1) */)
-  // );
 };
--- music-dapp/react/src/ipfs/upload.js
+++ music-dapp/react/src/ipfs/upload.js
@@ -1,7 +1,15 @@
-const { create, CID } = require("ipfs-http-client");
+const { create } = require("ipfs-http-client");
 
+/*
+ * Upload Audio to IPFS.
+ *
+ * @param[in] data: The audio file content.
+ * @param[in] path: The path on the IPFS.
+ * @param[in] cb: The callback function for later use.
+ */
 export const uploadAudioIPFS = async (data, path = "night.mp3", cb) => {
   // connect to the default API address http://localhost:5001
+  // const node = create('http://host.docker.internal:5001');
   const node = create("http://127.0.0.1:5001");
 
   let added = await node.add({
@@ -12,5 +20,4 @@ export const uploadAudioIPFS = async (data, path = "night.mp3", cb) => {
   console.log("Added file:", added.path, added.cid.toString());
 
   cb(added.cid.toString());
-  // setHash();
 };
